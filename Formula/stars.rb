7269b63edaf50335a6b4d443ff6df5128f979293
gkze <george.kontridze@gmail.com>
2019-02-05 13:54:53 -0800

    Brew formula update for stars version v0.4.8

--- Formula/stars.rb
+++ Formula/stars.rb
@@ -1,9 +1,9 @@
 class Stars < Formula
   desc "Explore your Github stars"
   homepage "https://github.com/gkze/stars"
-  url "https://github.com/gkze/stars/releases/download/v0.4.7/stars_0.4.7_Darwin_x86_64.tar.gz"
-  version "0.4.7"
-  sha256 "2819c9917eef2fa0b7bacd30c588ad22686f80e75409e28838578099853b0764"
+  url "https://github.com/gkze/stars/releases/download/v0.4.8/stars_0.4.8_Darwin_x86_64.tar.gz"
+  version "0.4.8"
+  sha256 "d263c5d7b9977cd0754f8791bddff1dd5f7e343d1f7bf139b5aa5e145dc6acde"
 
   def install
     bin.install "stars"

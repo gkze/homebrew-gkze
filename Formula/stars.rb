4620d6fc3e393203aedd5a905ce2f84e52972a59
gkze <george.kontridze@gmail.com>
2022-04-07 10:47:21 -0700

    Brew formula update for stars version v0.19.24

--- Formula/stars.rb
+++ Formula/stars.rb
@@ -5,20 +5,20 @@
 class Stars < Formula
   desc "Explore your Github stars"
   homepage "https://github.com/gkze/stars"
-  version "0.17.24"
+  version "0.19.24"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gkze/stars/releases/download/v0.17.24/stars_0.17.24_Darwin_arm64.tar.gz"
-      sha256 "0fb364d190393ac71e74c29080d60c9b585be77bbb9dc47287f5c210007592fb"
+      url "https://github.com/gkze/stars/releases/download/v0.19.24/stars_0.19.24_Darwin_arm64.tar.gz"
+      sha256 "87bc85f1c8d8e47420c214cc6cd7bc639525d1740b7a8f7b1c7d2475199e4422"
 
       def install
         bin.install "stars"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/gkze/stars/releases/download/v0.17.24/stars_0.17.24_Darwin_x86_64.tar.gz"
-      sha256 "8f1bbfadee719522f8a53851813b8a58726c44116292763830a81a7ceb5052f3"
+      url "https://github.com/gkze/stars/releases/download/v0.19.24/stars_0.19.24_Darwin_x86_64.tar.gz"
+      sha256 "51979ce85e8c89e163418b74d19b0a35e81687066c35ea59db427452c708f7a1"
 
       def install
         bin.install "stars"
@@ -28,16 +28,16 @@ class Stars < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/gkze/stars/releases/download/v0.17.24/stars_0.17.24_Linux_arm64.tar.gz"
-      sha256 "d612bb1f78722d9f6f29a9c993104b48d9cc965ea985d89198f5cf2537541127"
+      url "https://github.com/gkze/stars/releases/download/v0.19.24/stars_0.19.24_Linux_arm64.tar.gz"
+      sha256 "897503f61678ec1eaa52bbc3f1d0caa55b63e13c999226f6cd271496d6ad8acc"
 
       def install
         bin.install "stars"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/gkze/stars/releases/download/v0.17.24/stars_0.17.24_Linux_x86_64.tar.gz"
-      sha256 "d6911305c6c274b6d753519bf53614b2eb73e7535b1d71ed97c1d7def4180005"
+      url "https://github.com/gkze/stars/releases/download/v0.19.24/stars_0.19.24_Linux_x86_64.tar.gz"
+      sha256 "ff2c8681cf385539d5990039d47873c08d8c30b2f224aa2c9a5ecbbaa2c59cb7"
 
       def install
         bin.install "stars"

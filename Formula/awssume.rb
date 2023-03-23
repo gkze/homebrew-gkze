4b99144409a853ad6258f5693fb5338c978b5c76
gkze <george.kontridze@gmail.com>
2023-03-22 18:03:00 -0700

    Brew formula update for awssume version v0.12.4

--- Formula/awssume.rb
+++ Formula/awssume.rb
@@ -5,11 +5,11 @@
 class Awssume < Formula
   desc "Go package to switch between AWS IAM Roles"
   homepage "https://github.com/gkze/awssume"
-  version "0.7.4"
+  version "0.12.4"
 
   on_macos do
-    url "https://github.com/gkze/awssume/releases/download/v0.7.4/awssume_0.7.4_Darwin_x86_64.tar.gz"
-    sha256 "f8c3172c7e3180e04547ad49bc983be5b6d4ebbb0bc24f90e955382d215c195d"
+    url "https://github.com/gkze/awssume/releases/download/v0.12.4/awssume_0.12.4_Darwin_x86_64.tar.gz"
+    sha256 "a3a93c0f0eb6b01c113eb1953809081e9fbd5599dbf264a4b1c2b26f63344af4"
 
     def install
       bin.install "awssume"
@@ -28,8 +28,8 @@ class Awssume < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/gkze/awssume/releases/download/v0.7.4/awssume_0.7.4_Linux_x86_64.tar.gz"
-      sha256 "626491d58dea34def0b2b67e61e2e1f5f821b1e7170162177f3339f933b9d415"
+      url "https://github.com/gkze/awssume/releases/download/v0.12.4/awssume_0.12.4_Linux_x86_64.tar.gz"
+      sha256 "d64b11cd3bd0d714ff7888030ae786f8cbb2e74bcfaccaabd5a51d6bb83218c4"
 
       def install
         bin.install "awssume"
